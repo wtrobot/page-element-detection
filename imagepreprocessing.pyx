4d8fdd091ef8ea1175f60c1e6867b7d901541e16
nik133 <nikhil@bonfleet.com>
2019-10-20 12:41:34 +0530

    changed kernel from 3X3 laplace kernel to 5x5 laplocian gaussian mask

--- imagepreprocessing.pyx
+++ imagepreprocessing.pyx
@@ -34,14 +34,14 @@ def readImage(image_path):
 
 cpdef np.ndarray[np.double_t,ndim=3] convolution3D(np.ndarray[np.double_t,ndim=3] img_arr,int row, int col,int channel,int padding=0,int stride=1):
     
-    cdef np.ndarray[np.double_t,ndim=2] V_kernel=np.array([(1.0,1.0,1.0),(1.0,-8.0,1.0),(1.0,1.0,1.0)])
+    cdef np.ndarray[np.double_t,ndim=2] V_kernel=np.array([(0.0,0.0,-1.0,0.0,0.0),(0.0,-1.0,-2.0,-1.0,0.0),(-1.0,-2.0,16.0,-2.0,-1.0),(0.0,-1.0,-2.0,-1.0,0.0),(0.0,0.0,-1.0,0.0,0.0)])
     cdef np.ndarray[np.double_t,ndim=2] H_kernel=V_kernel.T
-    cdef float r=floor((row+2*padding-V_kernel.shape[0]+1)/stride)
-    cdef float c=floor((col+2*padding-V_kernel.shape[1]+1)/stride)
+    cdef int r=floor((row+2*padding-V_kernel.shape[0]+1)/stride)
+    cdef int c=floor((col+2*padding-V_kernel.shape[1]+1)/stride)
     cdef np.ndarray[np.double_t,ndim=3] conv
-    conv = np.zeros([int(r),int(c),int(channel)],dtype=np.double)
+    conv = np.zeros([r,c,int(channel)],dtype=np.double)
     
-    cdef np.ndarray[np.double_t,ndim=3] seg_holder=np.zeros([V_kernel.shape[0],V_kernel.shape[1],int(channel)])
+    cdef np.ndarray[np.double_t,ndim=3] seg_holder=np.zeros([int(channel),V_kernel.shape[0],V_kernel.shape[1]])
     cdef int l=0
     cdef int k=0
     cdef int i
@@ -53,23 +53,23 @@ cpdef np.ndarray[np.double_t,ndim=3] convolution3D(np.ndarray[np.double_t,ndim=3
         for j in range(conv.shape[1]):
             k=i
             
-            for q in range(seg_holder.shape[0]):
+            for q in range(seg_holder.shape[1]):
                 if j!=0:
                     l= j + (stride-1)
                 else:
                     l=j
                 
                 for w in range (seg_holder.shape[1]):
-                    seg_holder[q][w][0]=img_arr[k][l][0]
-                    seg_holder[q][w][1]=img_arr[k][l][1]
-                    seg_holder[q][w][2]=img_arr[k][l][2]
+                    seg_holder[0][q][w]=img_arr[k][l][0]
+                    seg_holder[1][q][w]=img_arr[k][l][1]
+                    seg_holder[2][q][w]=img_arr[k][l][2]
                     l+=1
                 k+=1
                 
-            conv[i][j][0]=np.sum(np.multiply(seg_holder[:][0],V_kernel))
+            conv[i][j][0]=np.sum(np.multiply(seg_holder[0][:],V_kernel))
             
-            conv[i][j][1]=np.sum(np.multiply(seg_holder[:][1],V_kernel))
+            conv[i][j][1]=np.sum(np.multiply(seg_holder[1][:],V_kernel))
             
-            conv[i][j][2]=np.sum(np.multiply(seg_holder[:][2],V_kernel))
+            conv[i][j][2]=np.sum(np.multiply(seg_holder[2][:],V_kernel))
           
     return conv
